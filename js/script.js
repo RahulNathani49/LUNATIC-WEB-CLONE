03340eefbf329e6a5b63739e15c376188f8c8e41
RahulNathani49 <nathanirahul49@gmail.com>
2019-02-24 16:12:49 +0530

    lunaic

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function click1(){
+    var sec = document.getElementById('circ2').style.backgroundColor = "#dcdcdc";
+    document.getElementById('circ1').style.backgroundColor = "#1ea9fe";
+}
+function click2(){
+    var fir = document.getElementById('circ1').style.backgroundColor = "#dcdcdc";
+    
+    document.getElementById('circ2').style.backgroundColor = "#1ea9fe";
+    
+}
