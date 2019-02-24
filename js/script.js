4db706150e880b80c9f0c30e0207c735b008d600
RahulNathani49 <nathanirahul49@gmail.com>
2019-02-24 23:45:51 +0530

    lun

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ function click1(){
 function click2(){
     var fir = document.getElementById('circ1').style.backgroundColor = "#dcdcdc";
     
-    document.getElementById('circ2').style.backgroundColor = "#1ea9fe";
+    document.getElementById('circ2').style.backgroundColor = "#1ea9fe" ;
+    
+}
+
+function togg(){                                    document.getElementById('menubar').style.display = "none";
+
+ document.getElementById('cross').style.display = "block";
+
+document.getElementsByClassName('mobile-menu')[0].style.display = "block"; 
+
+}
+
+function crossclick(){
+    
+ document.getElementById('menubar').style.display = "block";
+    
+ document.getElementById('cross').style.display = "none"; 
+
+document.getElementsByClassName('mobile-menu')[0].style.display = "none";
+    
     
 }
